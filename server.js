70d6ccb0134690ea4068856e5cf6cca13f294672
Vinicius Santos <vinicius.silva@gabster.com.br>
2022-01-08 15:40:10 -0300

    Alter message root path.

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => res.json("API Running"));
+app.get("/", (req, res) => res.json({
+    message: "Deploy successfully."
+}));
 
 
 const PORT = process.env.PORT || 5000;
